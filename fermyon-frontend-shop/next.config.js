7a4f1df75b006c744b210635664a9dcddfc79028
Steven Newstead <steve@stevens-mbp.lan>
2023-04-15 00:28:54 +0100

    first list

--- fermyon-frontend-shop/next.config.js
+++ fermyon-frontend-shop/next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com']
-},
+    domains: ['images.unsplash.com'],
+  },
 }
-const nextConfig = {
-
-}
-
-module.exports = nextConfig
--- fermyon-frontend-shop/src/pages/index.tsx
+++ fermyon-frontend-shop/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import Image from 'next/image';
+import Image from "next/legacy/image";
 import { GetServerSideProps, NextPage } from 'next';
 
 type Product = {
@@ -13,6 +13,17 @@ type HomePageProps = {
   products: Product[];
 };
 
+export const getServerSideProps: GetServerSideProps<HomePageProps> = async () => {
+  const res = await axios.get('https://spin-http-js-oz6sxnhe.fermyon.app/api/products');
+  const products = res.data;
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
+
 
 
 const HomePage: NextPage<HomePageProps> = ({ products }) => {
@@ -20,18 +31,11 @@ const HomePage: NextPage<HomePageProps> = ({ products }) => {
     <div className="p-4">
       <h1 className="text-xl font-bold mb-4">Products</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          <div key="1" className="border border-gray-200 p-4 rounded">
-            <div className="w-full h-64 relative mb-4">
-              <Image
-                src="https://images.unsplash.com/photo-1611831928379-8b2b0b2b2b1f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-                alt="Product Image"
-                layout="fill"
-                objectFit="cover"
-                objectPosition="center"
-              />
-            </div>
-            <h2 className="text-lg font-semibold mb-2">Test 1</h2>
+      {products.map((product) => (
+          <div key={product.id} className="border border-gray-200 p-4 rounded">
+            <h2 className="text-lg font-semibold mb-2">{product.name}</h2>
           </div>
+        ))}
       </div>
     </div>
   );
